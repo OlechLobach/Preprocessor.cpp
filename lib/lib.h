c4049bb8d408184a5a2012b41c0653f807931bc7
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-14 19:59:06 +0200

    initiall commit

---
+++ lib/lib.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+#ifndef LIB_H
+#define LIB_H
+
+
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define SQUARE(x) ((x) * (x))
+#define POWER(x, n) (int)pow((x), (n))
+#define IS_EVEN(x) ((x) % 2 == 0)
+#define IS_ODD(x) ((x) % 2 != 0)
+
+#endif 
--- mian.cpp
+++ mian.cpp
@@ -1,6 +1,33 @@
-#include<iostream>
+#include "lib/lib.h"
 
-int main()
-{
-	return 0;
+int main() {
+    int a, b;
+
+    cout << "Enter the first number: ";
+    cin >> a;
+
+    cout << "Enter the second number: ";
+    cin >> b;
+
+    cout << "Min of " << a << " and " << b << " is: " << MIN(a, b) << endl;
+    cout << "Max of " << a << " and " << b << " is: " << MAX(a, b) << endl;
+    cout << "Square of " << a << " is: " << SQUARE(a) << endl;
+    cout << "Power of " << a << " to the 3rd: " << POWER(a, 3) << endl;
+
+    if (IS_EVEN(a)) {
+        cout << a << " is even." << endl;
+    }
+    else {
+        cout << a << " is odd." << endl;
+    }
+
+    if (IS_ODD(b)) {
+        cout << b << " is odd." << endl;
+    }
+    else {
+        cout << b << " is even." << endl;
+    }
+
+    return 0;
+}
 }
